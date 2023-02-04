1a8d7f8f7e30014fd9fd84f61760f2f7e35974f1
letsjo <hyunoh.jo@gmail.com>
2023-02-05 03:18:19 +0900

    feat(*): set recoil for controller Bottom Tab

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+
 import { NavigationContainer } from '@react-navigation/native';
-import StackNavigation from './navigations/StackNavigation';
+import BottomTabNavigation from './navigations/BottomTabNavigation';
+
+import { RecoilRoot } from 'recoil';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigation />
+      <RecoilRoot>
+        <BottomTabNavigation />
+      </RecoilRoot>
     </NavigationContainer>
   )
 }
---
+++ src/state/isTabViewState.js
@@ -0,0 +1,6 @@
+import { atom } from 'recoil';
+
+export const isTabViewState = atom({
+  key: 'isTabViewState',
+  default: true,
+});
